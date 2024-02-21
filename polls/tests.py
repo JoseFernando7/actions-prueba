af39c2742ffa4eef1834ffbfa955946787baaae5
JoseFernando7 <jose.f72000@gmail.com>
2024-02-20 22:10:53 -0500

    Tests added

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
+from .views import index, hello
 
 # Create your tests here.
+class ViewsTestCase(TestCase):
+  def test_index_view(self):
+    response = self.client.get(reverse('index'))
+    self.assertEquals(response.status_code, 200)
+    self.assertContains(response, "Hello, world. This change will make the test fail")
+
+  def test_hello_view(self):
+    response = self.client.get(reverse('hello'))
+    self.assertEquals(response.status_code, 200)
+    self.assertContains(response, 'El arroz con leche sabe a pollo')
--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('/hello', views.hello, name='greet'),
+  path('hello', views.hello, name='hello'),
 ]
